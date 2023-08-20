1fa4ae3bb7a771f9820044cf9ea0d444407f73b5
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2023-08-19 23:48:13 -0300

    add doc

---
+++ tasks/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework import routers
+from tasks.views import TaskView
+
+
+
+
+
+router = routers.DefaultRouter()
+router.register(r'tasks', TaskView, "tasks")
+
+urlpatterns = [
+    path('api/v1', include(router.urls)),
+    path("docs/", include_docs_urls(title="Tasks API", public=True)),
+]
