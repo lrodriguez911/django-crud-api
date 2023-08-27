18b301f865ad7058202ff89101bee19e671d3086
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2023-08-27 20:32:24 -0300

    add taskform

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,6 +1,22 @@
+import { useForm } from 'react-hook-form'
+
+
 export function TaskFormPage() {
+
+    const { register, handleSubmit } = useForm()
+
+    const onSubmit = handleSubmit(data => {
+      console.log(data);
+    })
+
     return (
-      <div>TaskFormPage</div>
+      <div>
+        <form onSubmit={onSubmit()}>
+          <input type="text" name="title" id="title" placeholder="Title" {...register("title", {required: true})}/>
+          <textarea name="description" id="description" cols="30" rows="10" placeholder="Description" {...register("description", {required: true})}></textarea>
+          <button>Save</button>
+        </form>
+      </div>
     )
   }
   
