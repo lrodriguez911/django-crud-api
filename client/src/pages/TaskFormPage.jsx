7b6ed9b014251161f8a080ab6b17cbad711e168e
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2023-08-28 23:47:06 -0300

    complete mehtod to create tasks

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,19 +1,24 @@
 import { useForm } from 'react-hook-form'
+import { createTask } from '../api/tasks.api';
+import { useNavigate } from 'react-router-dom';
 
 
 export function TaskFormPage() {
 
-    const { register, handleSubmit } = useForm()
-
-    const onSubmit = handleSubmit(data => {
-      console.log(data);
+    const { register, handleSubmit, formState: { errors} } = useForm()
+    const navigate = useNavigate()
+    const onSubmit =  handleSubmit(async data => {
+      await createTask(data)
+      navigate("/")
     })
 
     return (
       <div>
         <form onSubmit={onSubmit()}>
           <input type="text" name="title" id="title" placeholder="Title" {...register("title", {required: true})}/>
+          {errors.title && <p>Title is required</p>}
           <textarea name="description" id="description" cols="30" rows="10" placeholder="Description" {...register("description", {required: true})}></textarea>
+          {errors.description && <p>Description is required</p>}
           <button>Save</button>
         </form>
       </div>
